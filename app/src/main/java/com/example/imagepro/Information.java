4226f0e2f48f705b1ae222a81d24f7b57b4cd27e
Aftab0023 <162977501+Aftab0023@users.noreply.github.com>
2024-03-18 23:31:16 +0530

    Images Added

--- app/src/main/java/com/example/imagepro/Information.java
+++ app/src/main/java/com/example/imagepro/Information.java
@@ -10,5 +10,9 @@ public class Information extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
     }
 }
--- app/src/main/java/com/example/imagepro/Learn.java
+++ app/src/main/java/com/example/imagepro/Learn.java
@@ -1,13 +1,35 @@
 package com.example.imagepro;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Learn extends AppCompatActivity {
 
+    TextView learn_more;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        learn_more = findViewById(R.id.learnmore);
+        learn_more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri you=Uri.parse("https://youtube.com/@LearnHowtoSign?si=au0SnySQA0WmWsSN");
+                Intent you_intent=new Intent(Intent.ACTION_VIEW,you);
+                startActivity(you_intent);
+
+            }
+        });
     }
 }
